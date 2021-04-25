13b902b9665bc682ccf685666560b18afed49ea2
Tom Duncalf <tom@tomduncalf.com>
2021-04-25 14:30:38 +0100

    Colourize log level

--- src/logging/Log.ts
+++ src/logging/Log.ts
@@ -1,7 +1,5 @@
 import { loggingConfig } from "src/config/loggingConfig";
 
-const MODULE_STYLE = "color: #999";
-
 type Module = keyof typeof loggingConfig.modules;
 
 export class Log {
@@ -9,16 +7,15 @@ export class Log {
     this.module = module;
   }
 
-  get moduleArgs() {
-    return [`%c${this.module}`, MODULE_STYLE];
+  private logArgs(level: string) {
+    return [`%c${this.module} %c${level}`, "color: #777", "color: #999"];
   }
-
   trace = (...logItems: any[]) => {
     if (
       loggingConfig.enabled.trace &&
       loggingConfig.modules[this.module].trace
     ) {
-      console.log(...this.moduleArgs, "TRACE", ...logItems);
+      console.log(...this.logArgs("TRACE"), ...logItems);
     }
   };
 
@@ -27,19 +24,19 @@ export class Log {
       loggingConfig.enabled.debug &&
       loggingConfig.modules[this.module].debug
     ) {
-      console.log(...this.moduleArgs, "DEBUG", ...logItems);
+      console.log(...this.logArgs("DEBUG"), ...logItems);
     }
   };
 
   warn = (...logItems: any[]) => {
     if (loggingConfig.enabled.warn && loggingConfig.modules[this.module].warn) {
-      console.warn(...this.moduleArgs, "WARN", ...logItems);
+      console.warn(...this.logArgs("WARN"), ...logItems);
     }
   };
 
   error = (...logItems: any[]) => {
     if (loggingConfig.enabled.error) {
-      console.error(...this.moduleArgs, "ERROR", ...logItems);
+      console.error(...this.logArgs("ERROR"), ...logItems);
     }
   };
 }
